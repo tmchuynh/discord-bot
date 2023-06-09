62219cbf4116743505dab8668aa1aa7313dab6cd
tmchuynh <tinamchuynh@gmail.com>
2023-06-09 12:12:32 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import { REST, Routes } from 'discord.js';
-import { clientId, guildId, token } from './config.json';
+// import { clientId, guildId, token } from './config.json';
 import { readdirSync } from 'node:fs';
-import { join } from 'node:path';
+// import { join } from 'node:path';
+
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
+// const foldersPath = join(__dirname, 'commands');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const foldersPath = join(__dirname, 'commands');
 const commandFolders = readdirSync(foldersPath);
 
@@ -13,6 +20,7 @@ for (const folder of commandFolders) {
 	const commandsPath = join(foldersPath, folder);
 	const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    console.log(`Number of command folders: ${commandFolders.length}`);
 	for (const file of commandFiles) {
 		const filePath = join(commandsPath, file);
 		const command = require(filePath);
@@ -22,10 +30,11 @@ for (const folder of commandFolders) {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
+    console.log(`Number of commands: ${commands.length}`);
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 // and deploy your commands!
 (async () => {
@@ -34,7 +43,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 			{ body: commands },
 		);
 
